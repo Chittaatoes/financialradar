e674af07b2d30bfd7553b9aa42ac69f53d6b6f14
Chittaatoes <hepyrafsanjaya01@gmail.com>
2026-03-08 12:39:28 +0700

    fix: cross-site cookie for oauth

--- backend/src/auth/index.ts
+++ backend/src/auth/index.ts
@@ -24,7 +24,7 @@ export async function setupAuth(app: Express) {
   saveUninitialized: false,
   rolling: true,
   cookie: {
-    secure: process.env.NODE_ENV === "production",
+    secure: true",
     httpOnly: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
